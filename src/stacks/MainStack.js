9566be1994c2702768bc5f434643213d9cb10ec4
Vinicius Martins <vinimartdev@gmail.com>
2020-10-31 19:41:33 -0300

    Route correction

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -8,9 +8,15 @@ import SignUp from '../screens/SignUp';
 const Stack = createStackNavigator();
 
 export default () => {
-  <Stack.Navigator>
-    <Stack.Screen name="Preload" component={Preload} />
-    <Stack.Screen name="SignIn" component={SignIn} />
-    <Stack.Screen name="SignUp" component={SignUp} />
-  </Stack.Navigator>;
+  return (
+    <Stack.Navigator
+      initialRouteName="Preload"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Preload" component={Preload} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
 };
